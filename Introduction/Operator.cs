5f4555fdcedc571c433b9d62bab712db94667040
HeroPei <peihaojiede@gmail.com>
2025-10-10 16:43:11 +0800

    运算符

---
+++ Introduction/Operator.cs
@@ -0,0 +1,13 @@
+using Xunit.Abstractions;
+
+namespace 入门;
+
+/// <summary>
+/// 运算符
+/// </summary>
+public class Operator : MyBasePrintClass
+{
+    public Operator(ITestOutputHelper output) : base(output)
+    {
+    }
+}
