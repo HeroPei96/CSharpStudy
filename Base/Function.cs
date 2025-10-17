7c8b4beafa5ee862ca66a3e56ced006ebe7abd3a
HeroPei <peihaojiede@gmail.com>
2025-10-17 12:16:08 +0800

    params 变长参数

--- Base/Function.cs
+++ Base/Function.cs
@@ -5,6 +5,7 @@ namespace 基础;
 /// <summary>
 /// 函数
 /// ref & out 在函数中的使用
+/// params 变长参数
 /// </summary>
 public class Function : MyBasePrintClass
 {
@@ -50,5 +51,26 @@ public class Function : MyBasePrintClass
         //out 修饰的参数必须在函数内初始化
         value = 2;
         value = value * 2;
+    }
+
+    /// <summary>
+    /// 用于传入多个参数(可变参数长度)
+    /// params 变长参数 一定是最后的一组参数
+    /// params 变长参数 只能有一个
+    /// </summary>
+    [Fact(DisplayName = "params 变长参数")]
+    public void Test()
+    {
+        ParamsMethod("HeroP", 2, 4, "abc", "123", "qwe");
+    }
+
+    private void ParamsMethod(string name, int a, int b, params string[] strs)
+    {
+        output.WriteLine($"我的名字是: {name}");
+        output.WriteLine($"a + b = {a + b}");
+        for (int i = 0; i < strs.Length; i++)
+        {
+            output.WriteLine($"str{i}: {strs[i]}");
+        }
     }
 }
