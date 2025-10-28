d71700ea39e27d082fa72d7b8a99f8aaf3c8c078
HeroPei <peihaojiede@gmail.com>
2025-10-28 15:54:33 +0800

    Dictionary - 泛型功能的 HashTable

---
+++ Advance/Dictionary.cs
@@ -0,0 +1,23 @@
+namespace 进阶;
+
+/// <summary>
+/// Dictionary - 本质上是有泛型功能的 HashTable
+/// </summary>
+public class Dictionary : MyBasePrintClass
+{
+    public Dictionary(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [Fact(DisplayName = "增删查改")]
+    public void Test1()
+    {
+        Dictionary<string, string> dictionary = new Dictionary<string, string>();
+        dictionary.Add("key1", "value1");
+        dictionary.Add("key2", "value2");
+        dictionary.Add("key3", "value4");
+
+        // 与 HastTable 不同的是，获取一个不存在的键会报错，HashTable 会返回空
+        // string str = dictionary["key4"];
+    }
+}
