68c3c8f08adbf908c61c6d81ac6ee2add639a982
HeroPei <peihaojiede@gmail.com>
2025-10-28 15:30:49 +0800

    List - 泛型功能的 ArrayList

---
+++ Advance/ListT.cs
@@ -0,0 +1,22 @@
+namespace 进阶;
+
+/// <summary>
+/// List - 本质上是有泛型功能的 ArrayList
+/// </summary>
+public class ListT : MyBasePrintClass
+{
+    public ListT(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [Fact(DisplayName = "增删查改")]
+    public void Test1()
+    {
+        List<string> list = new List<String>();
+        list.Add("123");
+        list.Add("456");
+        list.Add("qwe");
+        list.Add("qaz");
+        list.Add("asd");
+    }
+}
