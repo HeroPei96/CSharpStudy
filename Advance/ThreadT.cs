4306e7baec4087f35dc008d3a0388b62d59e8591
HeroPei <peihaojiede@gmail.com>
2025-10-29 17:34:10 +0800

    多线程 - 线程的启动和停止

---
+++ Advance/ThreadT.cs
@@ -0,0 +1,12 @@
+namespace 进阶;
+
+/// <summary>
+/// 多线程
+/// 见 Test 模块下 ThreadTest
+/// </summary>
+public class ThreadT : MyBasePrintClass
+{
+    public ThreadT(ITestOutputHelper output) : base(output)
+    {
+    }
+}
--- Test/MainTest.cs
+++ Test/MainTest.cs
@@ -4,6 +4,12 @@ public class MainTest
 {
     public static void Main(string[] args)
     {
-        
+        ThreadTest();
+    }
+
+    public static void ThreadTest()
+    {
+        //线程的启动和停止
+        new ThreadTest().Test1();
     }
 }
