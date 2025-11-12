8d2c6dd61978cf5d8d77417e5905987125faad8a
HeroPei <peihaojiede@gmail.com>
2025-11-12 22:42:45 +0800

    循环测试

--- Introduction/Loop.cs
+++ Introduction/Loop.cs
@@ -3,7 +3,24 @@ namespace 入门;
 /// <summary>
 /// 循环
 /// </summary>
-public class Loop
+public class Loop : MyBasePrintClass
 {
-    
+    public Loop(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [Fact(DisplayName = "数组循环 - 当数组容量为0时")]
+    public void Test1()
+    {
+        int[] array = new int[0];
+        for (var i = 0; i < array.Length; i++)
+        {
+            output.WriteLine($"array[{i}] = {array[i]}");
+        }
+
+        foreach (int i in array)
+        {
+            output.WriteLine($"item = {array[i]}");
+        }
+    }
 }
